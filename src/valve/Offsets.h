2d0a60ba06d87526873397b16b231fc589fa6aa7
Exlodium <45338028+Exlodium@users.noreply.github.com>
2023-11-04 03:34:18 +0100

    Up to date 03/11

--- src/valve/Offsets.h
+++ src/valve/Offsets.h
@@ -2,11 +2,11 @@
 
 namespace Offsets
 {
-    inline constexpr std::ptrdiff_t dwGlobalVars = 0x16AB2E0;
+    inline constexpr std::ptrdiff_t dwGlobalVars = 0x16AC100;
 
-    inline constexpr std::ptrdiff_t dwEntityList = 0x17AA8F8;
-    inline constexpr std::ptrdiff_t dwLocalPlayerController = 0x17F9C18;
-    inline constexpr std::ptrdiff_t dwViewMatrix = 0x1899080;
+    inline constexpr std::ptrdiff_t dwEntityList = 0x17AB958;
+    inline constexpr std::ptrdiff_t dwLocalPlayerController = 0x17FAC68;
+    inline constexpr std::ptrdiff_t dwViewMatrix = 0x180A0D0;
 
     inline constexpr std::ptrdiff_t dwGameSceneNode = 0x310;
     inline constexpr std::ptrdiff_t dwModelState = 0x160;
@@ -20,18 +20,18 @@ namespace Offsets
     
     inline constexpr std::ptrdiff_t m_iTeamNum = 0x3BF;
 
-    inline constexpr std::ptrdiff_t m_ArmorValue = 0x1500;
+    inline constexpr std::ptrdiff_t m_ArmorValue = 0x1508;
     inline constexpr std::ptrdiff_t m_iHealth = 0x32C;
 
-    inline constexpr std::ptrdiff_t m_flFlashMaxAlpha = 0x144C;
-    inline constexpr std::ptrdiff_t m_flFlashDuration = 0x1450;
+    inline constexpr std::ptrdiff_t m_flFlashMaxAlpha = 0x1464;
+    inline constexpr std::ptrdiff_t m_flFlashDuration = 0x1468;
 
     inline constexpr std::ptrdiff_t m_vOldOrigin = 0x1224;
-    inline constexpr std::ptrdiff_t m_vecLastClipCameraPos = 0x1284;
+    inline constexpr std::ptrdiff_t m_vecLastClipCameraPos = 0x128C;
 
-    inline constexpr std::ptrdiff_t m_angEyeAngles = 0x1508;
+    inline constexpr std::ptrdiff_t m_angEyeAngles = 0x1510;
 
-    inline constexpr std::ptrdiff_t m_entitySpottedState = 0x1628;
+    inline constexpr std::ptrdiff_t m_entitySpottedState = 0x1630;
 
     inline constexpr std::ptrdiff_t m_bHasDefuser = 0x40;
     inline constexpr std::ptrdiff_t m_bHasHelmet = 0x41;
@@ -40,8 +40,8 @@ namespace Offsets
     inline constexpr std::ptrdiff_t m_pInGameMoneyServices = 0x6D0;
     inline constexpr std::ptrdiff_t m_pItemServices = 0x10B0;
 
-    inline constexpr std::ptrdiff_t m_bIsScoped = 0x1398;
-    inline constexpr std::ptrdiff_t m_bIsDefusing = 0x13A0;
+    inline constexpr std::ptrdiff_t m_bIsScoped = 0x13A0;
+    inline constexpr std::ptrdiff_t m_bIsDefusing = 0x13A8;
 
     inline constexpr std::ptrdiff_t m_iAccount = 0x40;
     inline constexpr std::ptrdiff_t m_iStartAccount = 0x44;
